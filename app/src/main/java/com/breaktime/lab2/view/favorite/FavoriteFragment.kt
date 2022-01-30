f76d35e123758f7b7d06d17106bef613a26ad6a3
Elisey Budnitsky <bgs6544@gmail.com>
2022-01-30 14:09:35 +0300

    chore: add blank fragments

---
+++ app/src/main/java/com/breaktime/lab2/view/favorite/FavoriteFragment.kt
@@ -0,0 +1,18 @@
+package com.breaktime.lab2.view.favorite
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.breaktime.lab2.R
+
+class FavoriteFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_favorite, container, false)
+    }
+}
