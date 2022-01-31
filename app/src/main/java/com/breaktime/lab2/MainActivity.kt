7747dd7ab6318e93602c3303fffb774a6b4c89e8
Elisey Budnitsky <bgs6544@gmail.com>
2022-01-31 12:23:51 +0300

    feat: add bottom nav bar

--- app/src/main/java/com/breaktime/lab2/MainActivity.kt
+++ app/src/main/java/com/breaktime/lab2/MainActivity.kt
@@ -2,13 +2,22 @@ package com.breaktime.lab2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.breaktime.lab2.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(
+            this, R.layout.activity_main)
+        setContentView(binding.root)
         supportActionBar?.hide()
+        var nav = findNavController(R.id.fragmentContainerView)
+        binding.navbar.setupWithNavController(nav)
     }
 }
