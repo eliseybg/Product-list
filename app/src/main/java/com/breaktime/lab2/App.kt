aadc9668d7f30304e2e9c33349b3a691bcf66388
Elisey Budnitsky <bgs6544@gmail.com>
2022-02-01 06:34:15 +0300

    feat: add night mode

--- app/src/main/java/com/breaktime/lab2/App.kt
+++ app/src/main/java/com/breaktime/lab2/App.kt
@@ -1,8 +1,22 @@
 package com.breaktime.lab2
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+import com.breaktime.lab2.util.Preferences
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class App: Application() {
+class App : Application() {
+    @Inject
+    lateinit var preferences: Preferences
+
+    override fun onCreate() {
+        super.onCreate()
+        if (preferences.getNightMode()) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+    }
 }
--- app/src/main/java/com/breaktime/lab2/di/Module.kt
+++ app/src/main/java/com/breaktime/lab2/di/Module.kt
@@ -1,15 +1,31 @@
 package com.breaktime.lab2.di
 
+import android.content.Context
 import com.breaktime.lab2.currency_parser.CurrencyParser
+import com.breaktime.lab2.util.Preferences
+import com.breaktime.lab2.util.ResourcesProvider
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.FragmentComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
 
 @Module
 @InstallIn(FragmentComponent::class)
-object AnalyticsModule {
+object FragmentModule {
 
     @Provides
-    fun provideAnalyticsService() = CurrencyParser()
+    fun provideCurrencyParser() = CurrencyParser()
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    fun provideResourcesProvider(@ApplicationContext context: Context) = ResourcesProvider(context)
+
+    @Provides
+    fun providePreferences(@ApplicationContext context: Context) = Preferences(context)
 }
